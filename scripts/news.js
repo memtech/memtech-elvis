3fc45d8876c0b64eabcd227a196ffdf5f7c55507
Carter Capocaccia <carter.capocaccia@flyreel.co>
2020-10-16 07:31:40 -0500

    News Bug Fixes (#82)
    * adds news gathering by generic technology topic or specified topic
    
    * resolves bug with date strings and keyword argument going undefined

--- scripts/news.js
+++ scripts/news.js
@@ -14,6 +14,7 @@ const getNews = require('../helper/news.js')
 module.exports = robot => {
     robot.respond(/(news)\s*(\w*)/i, (res) => {
         keyword = res.match[2].trim()
+        keyword = keyword.length > 0 ? keyword : "technology"
         getNews(robot, keyword)
         .then((data) => {
             res.reply(JSON.stringify(data))
