fda52046eb217db2441464b6e9f7df434ca27bc7
Tilley <tilley@preteckt.com>
2020-10-23 16:08:08 -0500

    Fixed catfact, added catbreed

--- scripts/cat_fact.js
+++ scripts/cat_fact.js
@@ -17,11 +17,30 @@ module.exports = function (robot) {
   robot.respond(/catfact/i, function(msg) {
     return msg.http(`https://catfact.ninja/fact?max_length=140`)
       .get()(function(err, res, body) {
+        let jsonBody = JSON.parse(body)
         if (err) {
           return msg.send(`The doggos stole the catfacts!`);
         } else {
-          return msg.send(res.facts[0]);
+          return msg.send(jsonBody.fact);
         }
       });
   });
+
+  robot.respond(/catbreed/i, function(msg) {
+    return msg.http(`https://catfact.ninja/breeds?limit=1`)
+      .get()(function(err, res, body) {
+        const data = JSON.parse(body)
+        if (err) {
+          return msg.send(`Silly hooman! Only doggos have breeds!`);
+        } else {
+          let breed = data.breed
+          let country = data.country
+          let origin =  data.origin
+          let coat = data.coat
+          let pattern = data.pattern
+          let reply = `Today's Cat Breed is ${breed}!\nThey are from ${country}, have a ${origin} origin and a ${coat} coat with ${pattern} patterns.`
+          return msg.send(reply)
+        }
+      })
+  })
 }
